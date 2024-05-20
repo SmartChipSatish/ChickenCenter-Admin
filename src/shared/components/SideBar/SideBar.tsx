c2779cb100670e368a15a9f0cf24f24809519fa7
vishnuvardhan9901 <vishnuvardhan.vemula@onpassive.com>
2024-05-20 20:08:15 +0530

    test

--- src/shared/components/SideBar/SideBar.tsx
+++ src/shared/components/SideBar/SideBar.tsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 const SideBar = () => {
     const navigation = useNavigate();
     return <>
-        <div className="bg-primary text-white">
+        <div className="bg-primary text-white h-100">
             <p onClick={() => {
                 navigation('orders')
             }}>Orders</p>
