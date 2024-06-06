ba387eb4c01ed3cf063d1c7ea1aedd12106541d2
siddam sai vamsee prasad <vamsee.siddam@onpassive.com>
2024-06-06 16:50:42 +0530

    Updates

---
+++ assets/DashboardLayout-huJG72B4.js
@@ -0,0 +1,1 @@
+import{j as a,O as e}from"./index-DuJGvacx.js";const t=()=>a.jsxs(a.Fragment,{children:[a.jsx("p",{className:"pageTile pageTitleSpace",children:"DashBoard"}),a.jsx(e,{})]});export{t as default};
