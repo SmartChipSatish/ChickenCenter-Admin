f456d0d29b4cfdb0894e5a7dffa9dea56dee92dc
siddam sai vamsee prasad <vamsee.siddam@onpassive.com>
2024-06-06 16:26:00 +0530

    build

--- dist/assets/DashboardLayout-BWIDUH-5.js
+++
@@ -1,1 +0,0 @@
-import{j as a,O as e}from"./index-Zq6-1N3V.js";const t=()=>a.jsxs(a.Fragment,{children:[a.jsx("p",{className:"pageTile pageTitleSpace",children:"DashBoard"}),a.jsx(e,{})]});export{t as default};
