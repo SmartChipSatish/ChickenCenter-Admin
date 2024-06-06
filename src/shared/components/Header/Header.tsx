5d6d4816ecea1688ac1d7afa4a522f3f27022cda
siddam sai vamsee prasad <vamsee.siddam@onpassive.com>
2024-06-06 16:04:48 +0530

    changes

--- src/shared/components/Header/Header.tsx
+++ src/shared/components/Header/Header.tsx
@@ -48,7 +48,7 @@ const Header = () => {
                                 // account
                                 localStorage.clear();
                                 // navigation("login")
-                                window.location.href='ChickenCenter-Admin/login'
+                                window.location.href='/login'
                             }}> <FontAwesomeIcon icon={faSignOut} className='profileOptions'></FontAwesomeIcon>  Logout</Dropdown.Item>
                         </Dropdown.Menu>
                     </Dropdown>
